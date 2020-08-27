5602f3839bf1456ab84537de69fca3232e6b8fdb
git@end.re <git@end.re>
2020-08-27 13:04:35 +0200

    some additions

--- mqtt2atem/index.js
+++ mqtt2atem/index.js
@@ -1,44 +1,49 @@
+const atemAddress = '44.128.4.58';
+const baseAddr = '/mqtt2atem/'+atemAddress+'/';
+
 const { Atem } = require('atem-connection')
 const myAtem = new Atem({ externalLog: console.log })
-myAtem.connect('44.128.4.69')
-
-const mqttlib = require('mqtt')
-const mqtt = mqttlib.connect('mqtt://44.128.7.255')
-const mqttWildcard = require('mqtt-wildcard');
+myAtem.connect(atemAddress);
 
 const EventEmitter = require('events');
 class AtemEventsEmitter extends EventEmitter {}
 const AtemEvent = new AtemEventsEmitter();
 
-class MqttTopicEmitter extends EventEmitter {}
-const MqttTopic = new MqttTopicEmitter();
-
-//override addListener so we will subscribe to the requested MQTT topic(s) at the same time
-MqttTopic.on = (function (original) {
-    return function (name, callback) {
-        mqtt.subscribe(name);
-        console.log('subscribing',name);
-        return original(name, callback);
-    }
-})(MqttTopic.on.bind(MqttTopic));
-
+// Set up MQTT
+const MqttTopic = require('mqtt-wildcard-emitter');
+MqttTopic.connect('mqtt://44.128.7.255');
 //for traversing objects like paths (that has been given by ATEM state listener)
+//
 const objectPath = require("object-path");
 
 //global variable for reference if the multiview display is show or not
 var multiViewVisible = false;
 
+inputByIdMappings = {}
+inputByLongNameMappings = {}
+macroByIdMappings = {}
+macroByNameMappings = {}
+
+const DefaultEventHandler = function(key, value) {
+    if (key.substr(0,7) === 'inputs.') {
+        inputByIdMappings[value['inputId']] = value;
+        inputByLongNameMappings[value['longName']] = value;
+    } else if (key.substr(0,22) === 'macro.macroProperties.') {
+        if (value['isUsed']) {
+            macroByIdMappings[value['macroIndex']] = value;
+            macroByNameMappings[value['name']] = value;
+            MqttTopic.mqttclient.publish(baseAddr+'macro/'+String(value['macroIndex'])+'/name', value['name']);
+        }
+    } else
+      console.log('changed:', key, value);
+}
+
 myAtem.on('stateChanged', function(err, key) {
   //prevent event polluting with unsubscribed events
   if (key in AtemEvent._events)
     AtemEvent.emit(key, key, objectPath.get(myAtem.state, key));
-});
-
-mqtt.on('message', (topic, message) => {
-    MqttTopic.eventNames().forEach(function(topicMask) {
-        if (mqttWildcard(topic, topicMask))
-            MqttTopic.emit(topicMask, topic, message.toString());
-    });
+  else
+    DefaultEventHandler(key, objectPath.get(myAtem.state, key));
 });
 
 MqttTopic.on('/touchbox2mqtt/+/longtap', function(topic, value) {
@@ -55,11 +60,48 @@ MqttTopic.on('/touchbox2mqtt/3/longtap', function(topic, value) { myAtem.setAuxS
 MqttTopic.on('/touchbox2mqtt/4/longtap', function(topic, value) { myAtem.setAuxSource(4) });
 MqttTopic.on('/touchbox2mqtt/+/touched', function(topic, value) { myAtem.setAuxSource(9001) });
 
+
+//commands accepted from MQTT
+MqttTopic.on(baseAddr+'set/upstream_keyer_onair', function(topic, value) {
+    myAtem.setUpstreamKeyerOnAir(value=='1')
+});
+MqttTopic.on(baseAddr+'set/program_input_by_longname', function(topic, value) {
+    myAtem.changeProgramInput(inputByLongNameMappings[value]['inputId']);
+});
+MqttTopic.on(baseAddr+'set/preview_input_by_longname', function(topic, value) {
+    myAtem.changePreviewInput(inputByLongNameMappings[value]['inputId']);
+});
+MqttTopic.on(baseAddr+'macro/run_by_name', function(topic, value) {
+    myAtem.macroRun(macroByNameMappings[value]['macroIndex']);
+});
+
+//events sent to MQTT
 AtemEvent.on('video.auxilliaries.0', function(path, val) {
     //store this in the global variable
     multiViewVisible = val == 9001;
-    mqtt.publish('/atem2mqtt/44.128.4.69/'+path.replace(/\./g,'/'), JSON.stringify(val));
-    console.log('Multiview visible: ', multiViewVisible);
+    MqttTopic.mqttclient.publish(baseAddr+'multiview_visible', multiViewVisible && '1' || '0');
+    MqttTopic.mqttclient.publish(baseAddr+path.replace(/\./g,'/'), JSON.stringify(val));
+    MqttTopic.mqttclient.publish(baseAddr+'aux_out',String(val));
 });
+
+AtemEvent.on('video.ME.0.programInput', function(path, val) {
+    MqttTopic.mqttclient.publish(baseAddr+path.replace(/\./g,'/'), JSON.stringify(val));
+    MqttTopic.mqttclient.publish(baseAddr+'program', JSON.stringify(val));
+    MqttTopic.mqttclient.publish(baseAddr+'program_with_longname', inputByIdMappings[val]['longName']);
+    MqttTopic.mqttclient.publish(baseAddr+'program_with_shortname', inputByIdMappings[val]['shortName']);
+});
+
+AtemEvent.on('video.ME.0.previewInput', function(path, val) {
+    MqttTopic.mqttclient.publish(baseAddr+path.replace(/\./g,'/'), JSON.stringify(val));
+    MqttTopic.mqttclient.publish(baseAddr+'preview', JSON.stringify(val));
+    MqttTopic.mqttclient.publish(baseAddr+'preview_with_longname', inputByIdMappings[val]['longName']);
+    MqttTopic.mqttclient.publish(baseAddr+'preview_with_shortname', inputByIdMappings[val]['shortName']);
+});
+
+AtemEvent.on('video.ME.0.upstreamKeyers.0.onAir', function(path, val) {
+    MqttTopic.mqttclient.publish(baseAddr+path.replace(/\./g,'/'), val && '1' || '0');
+    MqttTopic.mqttclient.publish(baseAddr+'pip', val && '1' || '0');
+});
+
 
 console.log(Object.keys(AtemEvent._events));
--- synergy2mqtt/index.js
+++ synergy2mqtt/index.js
@@ -1,11 +1,24 @@
 const serverAddress = '44.128.4.3';
-const switchMatcher=new RegExp(/INFO: switch from "(\S+)" to "(\S+)"/);
+const baseAddr = '/synergy2mqtt/'+serverAddress+'/';
 
+// Set up MQTT
+const MqttTopic = require('mqtt-wildcard-emitter');
+MqttTopic.connect('mqtt://44.128.7.255');
+
+// Set up Synergy
+const switchMatcher=new RegExp(/INFO: switch from "(\S+)" to "(\S+)"/);
 const child = require('child_process').execFile('/usr/bin/synergys', ('-f -c /home/e/.synergys.conf -a '+serverAddress).split(' ')); 
 
 child.stdout.on('data', function(data) {
     const match = data.toString().match(switchMatcher);
     if (match) {
         console.log('switched from', match[1], 'to', match[2]); 
+        MqttTopic.mqttclient.publish(baseAddr+'switch/from_to', match[1]+"\t"+match[2]);
+        MqttTopic.mqttclient.publish(baseAddr+'switched_from', match[1]);
+        MqttTopic.mqttclient.publish(baseAddr+'switched_to', match[2]);
+        MqttTopic.mqttclient.publish(baseAddr+'screen/'+match[1]+'/active', "0");
+        MqttTopic.mqttclient.publish(baseAddr+'screen/'+match[2]+'/active', "1");
+    } else {
+        console.log(data)
     }
 });
